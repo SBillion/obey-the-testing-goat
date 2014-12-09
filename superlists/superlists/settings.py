5d99f05cb0824981bdc4a08dcd6491f0faf1d3f9
Sébastien Billion <sebastien.billion@gmail.com>
2014-12-09 17:49:16 +0100

    Refactor home page view to use a template

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lists',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -51,6 +52,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'superlists.urls'
 
 WSGI_APPLICATION = 'superlists.wsgi.application'
+
 
 
 # Database
@@ -81,3 +83,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
